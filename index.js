d943e5636aae0138280ee604c55d7a7e6a20d437
Srijan Sahay Srivastava <70461251+SrijanSahaySrivastava@users.noreply.github.com>
2023-01-30 00:17:16 +0530

    index file

---
+++ index.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js')
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildVoiceStates,
+    Discord.GatewayIntentBits.MessageContent
+  ]
+})
+const fs = require('fs')
+const config = require('./config.json')
+
+client.config = require('./config.json')
+client.commands = new Discord.Collection()
+client.aliases = new Discord.Collection()
+client.emotes = config.emoji
+
+// taking command files
+fs.readdir('./commands/', (err, files) => {
+  if (err) return console.log('Could not find any commands!')
+  const jsFiles = files.filter(f => f.split('.').pop() === 'js')
+  if (jsFiles.length <= 0) return console.log('Could not find any commands!')
+  jsFiles.forEach(file => {
+    const cmd = require(`./commands/${file}`)
+    console.log(`Loaded ${file}`)
+    client.commands.set(cmd.name, cmd)
+    if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
+  })
+})
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} is ready to play music.`)
+})
+
+// taking messages and spliting
+client.on('messageCreate', async message => {
+  if (message.author.bot || !message.guild) return
+  const prefix = config.prefix
+  if (!message.content.startsWith(prefix)) return
+  const args = message.content.slice(prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase()
+  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+  if (!cmd) return
+  if (cmd.inVoiceChannel && !message.member.voice.channel) {
+    return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
+  }
+  try {
+    cmd.run(client, message, args)
+  } catch (e) {
+    console.error(e)
+    message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
+  }
+})
+
